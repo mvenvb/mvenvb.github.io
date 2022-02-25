6b1f50aeac097ae0aeb88af09282d8ff613f9765
mvenvb <vikkka97@mail.ru>
2022-02-25 20:56:59 +0300

    modified mobile version

--- js/menu.js
+++ js/menu.js
@@ -1,23 +1,24 @@
+// let menuBtn = document.querySelector('.menu__burger');
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
-menuBtn.addEventListener('click', function(){
-	menuBtn.classList.toggle('active');
-	menu.classList.toggle('active');
+menuBtn.addEventListener('click', function () {
+  menuBtn.classList.toggle('active');
+  menu.classList.toggle('active');
 })
 
 const menuItem = document.getElementsByClassName("menu__item");
 const lines = document.getElementsByClassName("line");
 
 
-  Array.prototype.forEach.call(menuItem, function(element) {
-    element.addEventListener('click', function() {
-        let itemId = this.id;
-        let line = document.getElementById(itemId.split('-')[1]);
-		
-		Array.from(lines).filter(el => el.classList.contains('visible')).forEach(el => el.classList.remove("visible"));
-		
-        line.classList.add('visible');
-		menuBtn.classList.toggle('active');
-		menu.classList.toggle('active');
-    });
+Array.prototype.forEach.call(menuItem, function (element) {
+  element.addEventListener('click', function () {
+    let itemId = this.id;
+    let line = document.getElementById(itemId.split('-')[1]);
+
+    Array.from(lines).filter(el => el.classList.contains('visible')).forEach(el => el.classList.remove("visible"));
+
+    line.classList.add('visible');
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
   });
+});
